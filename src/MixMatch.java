5dcc883bfe8f77a9c972a0ed03dfef3efacfe6fc
Mzamor71 <100431992+Mzamor71@users.noreply.github.com>
2025-01-29 01:54:58 +0000

    Assignment Commit #1

---
+++ src/MixMatch.java
@@ -0,0 +1,24 @@
+public class MixMatch{
+    public static void main(String[] args){
+        String  word_1      = "Discombobulate";
+        char    letter      = 'B';
+        short   number_1    = 10000;
+        int     number_2    = 500;
+        long    number_3    = 1351531563;
+        float   number_4    = 2;
+        double  number_5    = 2.33333333333;
+        boolean answer      = true;
+        
+        System.out.println("Here are examples of different data types in Java: ");
+        System.out.println("    " + word_1 + " <- String, stores a combination of characters");
+        System.out.println("    " + letter + " <- char, stores a single character");
+        System.out.println("    " + number_1 + " <- short, stores whole numbers from -32,768 to 32,767");
+        System.out.println("    " + number_2 + " <- int, stores whole numbers from -2,147,483,648 to 2,147,483,647");
+        System.out.println("    " + number_3 + " <- long, stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+        System.out.println("    " + number_4 + " <- float, stores decimal values with 6 to 7 decimal digits");
+        System.out.println("    " + number_5 + " <- double, stores decimal values with 15 to 16 decimal digits");
+        System.out.println("    " + answer +  " <- boolean, stores true or false values. 0/1");
+
+
+    }
+}
